6688463b83926a5b2f3d4d4b549d204eea4576b8
Anwesh Korram <45973330+Anwesh897@users.noreply.github.com>
2023-11-27 21:51:50 +0530

    some addtion

--- src/App.js
+++ src/App.js
@@ -1,110 +1,19 @@
 import logo from './logo.svg';
-import React from 'react';
+//import React from 'react';
 import './App.css';
-//import './deriv';
-// Smaller bundle size, dealing only with the low-level library
-import DerivAPI from '@deriv/deriv-api';
-import { find } from 'rxjs/operators'
-import WebSocket from 'ws';
+import Call from './call';
+import Put from './put';
 
-const app_id = 51526;
-const token = 'oTFzEY0IoR4p1ax';
 
 function App() {
   return (
     <div className="App">
-      <Purchase />
+      <header className='App-header'>
+        <Call />
+        <Put />
+      </header>
     </div>
   );
-}
-
-class Purchase extends React.Component {
-  render() {
-    function handleClick(e) {
-      e.preventDefault();
-
-      if (!token) {
-        console.error('DERIV_TOKEN environment variable is not set');
-        process.exit(1);
-      }
-
-      const api = new DerivAPI({ endpoint: 'green.derivws.com', lang: 'EN', app_id: app_id });
-
-      async function main() {
-        try {
-          const account = await api.account(token);
-
-          const { balance, currency } = account;
-
-          console.log(`Your current balance is: ${balance.currency} ${balance.display}`);
-
-          balance.onUpdate(() => {
-            console.log(`Your new balance is: ${balance.currency} ${balance.display}`);
-          });
-
-          const contract = await api.contract({
-            proposal: 1,
-            subscribe: 1,
-            symbol: "stpRNG",
-            contract_type: "MULTDOWN",
-            // contract_category: "multiplier",
-            multiplier: 5000,
-            currency: "USD",
-            basis: "stake",
-            amount: 50,
-          });
-
-          contract.onUpdate(({ status, payout, bid_price }) => {
-            switch (status) {
-              case 'proposal':
-                return console.log(
-                  `Current payout: ${payout.currency} ${payout.display}`,
-                );
-              case 'open':
-                return console.log(
-                  `Current bid price: ${bid_price.currency} ${bid_price.display}`,
-                );
-              default:
-                break;
-            }
-          });
-
-          // Wait until payout is greater than USD 19
-          // await contract.onUpdate()
-          //     .pipe(find(({ payout }) => payout.value >= expected_payout)).toPromise();
-
-          const buy = await contract.buy();
-          setInterval(function () {
-            return contract.sell(({ subscribe: 0 }))
-          }, 5000)
-
-
-          //console.log(buy.contract_id)
-
-          //console.log(`Buy price is: ${buy.price.currency} ${buy.price.display}`);
-
-          // Wait until the contract is sold 
-          await contract.onUpdate().pipe(find(({ is_sold }) => is_sold)).toPromise();
-
-          const { profit, status } = contract;
-
-          console.log(`You ${status}: ${profit.currency} ${profit.display}`);
-        } catch (err) {
-          console.error(err);
-        } finally {
-          // Close the connection and exit
-          api.basic.disconnect();
-        }
-      }
-      main()
-    }
-
-    return (
-      <header className="App-header">
-        <button className='purchase' onClick={handleClick}>purchase</button>
-      </header>
-    )
-  }
 }
 
 export default App;
